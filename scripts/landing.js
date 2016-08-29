46296e8cf8b7c4d3dc48d6a4ff55cb7afa92e720
Gamaliel Aguilar-Gamez <gama@9104studios.com>
2016-08-29 14:48:17 -0700

    Checkpoint 9 completed DOM scripting events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,19 @@
-var animatePoints = function () {
-                 
-    var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+}
+
+var animatePoints = function(points) {
+    forEach(points, revealPoint); 
+
     
-    var revealPoint = function(index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-    
-    for (var i = 0; i < points.length; i++) {
+    /* for (var i = 0; i < points.length; i++) {
         revealPoint(i);
-    }
+    } */
                                
 };
 
@@ -27,3 +29,21 @@ var animateTitle = function () {
     
     revealTitle();
 };
+
+window.onload = function () { //we added this function that enables things to happen after the whole DOM has been loaded by the browser.
+    //alert ("The window has loaded!");
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function (event) {
+        //console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + "pixels"); 
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    })
+}
+
