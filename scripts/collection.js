7c2dd197184dc9427a1d770cd4af01b95282923d
Gamaliel Aguilar-Gamez <gama@9104studios.com>
2016-09-07 23:55:12 -0700

    checkpoint 17 completed collection jquery refactor

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,6 @@
-var collectionItemTemplate =
+//#1 - refactor into jQuery - changed the name of the variable that stores the template from the original to 'template'.
+var buildCollectionItemTemplate = function () {
+    var template =
     '<div class="collection-album-container column fourth">'
     + '    <img src="assets/images/album_covers/01.png"/>'
     + '    <div class="collection-album-info caption">'
@@ -12,16 +14,24 @@ var collectionItemTemplate =
     + '        </p>'
     + '    </div>'
     + '</div>'
-;
-
-window.onload = function () {
-    //#1 
-    var collectionContainer = document.getElementsByClassName('album-covers')[0];
-    //#2 
-    collectionContainer.innerHTML = '';
+    ;
     
-    //#3
+    //#2 - here we wrap 'template' in a jQuery object in case we need to use jQuery methods in the future.
+    
+    return $(template);
+    
+};
+
+$(window).load(function () {
+    //#3 - replaced the DOM selector with the shorter jQuery equivalent.
+    var $collectionContainer = $('.album-covers');
+    //#4 - replaced the DOM scripting 'innerHTML' property with the jQuery 'empty()' method.
+    $collectionContainer.empty();
+    
+    
     for (var i = 0; i < 12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
+        var $newThumbnail = buildCollectionItemTemplate();
+        //#5 replace the += in the for loop with '.append' which does the same thing.
+        $collectionContainer.append($newThumbnail);
     }
-}
+});
